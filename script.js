9a3c4df235d2d14380f66183c3b2a6c2deacd428
catalincd <cc.dumitrescu02@gmail.com>
2021-09-01 20:05:20 +0300

    Init

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ var imgs = ["bmbjmp", "maze", "shoota", "tefm", "runner", "balance", "motion", "
 
 
 var json;
-$.getJSON("http://localhost:5000/", function(data){
-	json = data;
-	loadData(json);
+$.getJSON("https://raw.githubusercontent.com/catalincd/catalincd.github.io/main/res/games.json", function(data) {
+    json = data;
+    loadData(json);
 });
 
 
-function loadData(jsn){
-	for(var i=0;i<jsn.length;i++)
-	{
-	    var img = `images/${jsn[i].thumbnail}.png`;
-	    $("#cards").append(`<a href="#" class="card">
+function loadData(jsn) {
+    for (var i = 0; i < jsn.length; i++) {
+        var img = `images/${jsn[i].thumbnail}.png`;
+        $("#cards").append(`<a href="#" class="card">
 	    						<div class="glow"></div>
 	                            <img src="${img}">
 	                            <div class="bottomText">
 	                                <h1 class="h1Text">${jsn[i].name}</h1>
 	                            </div>
 	                        </a>`);
-	}
+    }
 }
 
 //
