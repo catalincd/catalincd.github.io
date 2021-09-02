465bc70523014f042e5572f250b7f2ca9419f671
catalincd <cc.dumitrescu02@gmail.com>
2021-09-02 10:37:43 +0300

    Skills

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-var imgs = ["bmbjmp", "maze", "shoota", "tefm", "runner", "balance", "motion", "blocks"];
-
-
-
-
 var json;
-$.getJSON("https://raw.githubusercontent.com/catalincd/catalincd.github.io/main/res/games.json", function(data) {
+//$.getJSON("https://raw.githubusercontent.com/catalincd/catalincd.github.io/main/res/data.json", function(data) {
+$.getJSON("http://localhost:5000/", function(data) {
     json = data;
-    loadData(json);
+    loadData(json.info);
+    loadSkills(json.skills)
+    loadGames(json.games);
 });
 
 
-function loadData(jsn) {
-    for (var i = 0; i < jsn.length; i++) {
-        var img = `images/${jsn[i].thumbnail}.png`;
+function loadSkills(skills) {
+    for (var i = 0; i < skills.length; i++) {
+        var lines = "";
+        for (var t = 0; t < skills[i].lines.length; t++) {
+            lines += `<div class="skill">
+                        <img src="images/icons/${skills[i].lines[t].icon}.png" class="skillIcon">
+                        <h3>${skills[i].lines[t].text}</h3>
+                    </div>`
+        }
+
+        var title = "";
+        //title = <h2>${skills[i].title}</h2>;
+
+        $("#skillsTable").append(`<div class="skillsColumn">${title}${lines}</div>`);
+    }
+}
+
+function loadData(info) {
+    for (var i = 0; i < info.length; i++) {
+        $("#info").append(`<h3 class="material-icons-outlined">${info[i].icon}</h3><h3>${info[i].text}</h3>`);
+    }
+}
+
+function loadGames(games) {
+    for (var i = 0; i < games.length; i++) {
+        var img = `images/${games[i].thumbnail}.png`;
         $("#cards").append(`<a href="#" class="card">
 	    						<div class="glow"></div>
 	                            <img src="${img}">
 	                            <div class="bottomText">
-	                                <h1 class="h1Text">${jsn[i].name}</h1>
+	                                <h1 class="h1Text">${games[i].name}</h1>
 	                            </div>
 	                        </a>`);
     }
 }
-
-//
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const port = 5000;
 const corsOptions = { origin: false };
 
 app.get('/', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-	var file = fs.readFileSync('./res/games.json');
-  	res.json(JSON.parse(file));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    var file = fs.readFileSync('./res/data.json');
+    res.json(JSON.parse(file));
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
